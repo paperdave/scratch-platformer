1d822f4853fabc8567e1d5abda479d75f1659ac3
dave caruso <dave@davecode.me>
2020-06-07 19:18:02 -0400

    fix bug where a star is impossible to get

--- import-downloads.sh
+++ import-downloads.sh
@@ -2,4 +2,7 @@ rm project.sb3 -f
 if [ -e ~/dl/project.sb3 ]; then mv ~/dl/project.sb3 ./project.sb3; fi
 if [ -e ~/Downloads/project.sb3 ]; then mv ~/Downloads/project.sb3 ./project.sb3; fi
 if [ -e ~/downloads/project.sb3 ]; then mv ~/downloads/project.sb3 ./project.sb3; fi
+if [ -e "~/downloads/Scratch Exploration Platformer.sb3" ]; then mv "~/downloads/Scratch Exploration Platformer.sb3" ./project.sb3; fi
+if [ -e "~/Downloads/Scratch Exploration Platformer.sb3" ]; then mv "~/Downloads/Scratch Exploration Platformer.sb3" ./project.sb3; fi
+if [ -e "~/dl/Scratch Exploration Platformer.sb3" ]; then mv "~/dl/Scratch Exploration Platformer.sb3" ./project.sb3; fi
 if [ -e ./project.sb3 ]; then ./unpack.sh; fi
